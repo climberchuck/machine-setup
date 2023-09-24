3d924173b1cec58ca64deb233e1da964c2cb883d
Chuck Bridgen <climberchuck@users.noreply.github.com>
2023-09-24 13:26:52 -0400

    added windows box script

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # machine-setup
+
+To execute, run the following from the directory:
+
+```PowerShell
+
+.\windows.ps1
+
+```
---
+++ windows.ps1
@@ -0,0 +1,92 @@
+#Install WinGet
+#Based on this gist: https://gist.github.com/crutkas/6c2096eae387e544bd05cde246f23901
+$hasPackageManager = Get-AppPackage -name 'Microsoft.DesktopAppInstaller'
+if (!$hasPackageManager -or [version]$hasPackageManager.Version -lt [version]"1.10.0.0") {
+    "Installing winget Dependencies"
+    Add-AppxPackage -Path 'https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx'
+
+    $releases_url = 'https://api.github.com/repos/microsoft/winget-cli/releases/latest'
+
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    $releases = Invoke-RestMethod -uri $releases_url
+    $latestRelease = $releases.assets | Where { $_.browser_download_url.EndsWith('msixbundle') } | Select -First 1
+
+    "Installing winget from $($latestRelease.browser_download_url)"
+    Add-AppxPackage -Path $latestRelease.browser_download_url
+}
+else {
+    "winget already installed"
+}
+
+#Configure WinGet
+Write-Output "Configuring winget"
+
+#winget config path from: https://github.com/microsoft/winget-cli/blob/master/doc/Settings.md#file-location
+$settingsPath = "$env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json";
+$settingsJson = 
+@"
+    {
+        // For documentation on these settings, see: https://aka.ms/winget-settings
+        "experimentalFeatures": {
+          "experimentalMSStore": true,
+        }
+    }
+"@;
+$settingsJson | Out-File $settingsPath -Encoding utf8
+
+
+#Install New apps
+Write-Output "Installing Apps"
+$apps = @(
+    <# General Applications #>
+    @{name = "CPUID.CPU-Z" }, 
+    @{name = "Discord.Discord" },
+    @{name = "Google.Chrome" },
+    @{name = "Nvidia.GeForceExperience" },
+    @{name = "Spotify.Spotify" },
+    @{name = "Valve.Steam" },
+
+    <# Productivity Applications #>
+    @{name = "Microsoft.Office" },
+    @{name = "Microsoft.Teams.Preview" },
+    @{name = "SlackTechnologies.Slack" },
+
+    <# Miscellaneous Applications #>
+    @{name = "Bitwarden.Bitwarden" },
+
+    <# Developer Languages #>
+    @{name = "Anaconda.Miniconda3" },
+    @{name = "Docker.DockerDesktop" },
+    @{name = "GitHub.GitHubDesktop" },
+    @{name = "Microsoft.DotNet.SDK.7" },
+    @{name = "Microsoft.PowerShell" },
+    @{name = "Microsoft.VisualStudioCode" },
+    @{name = "Microsoft.WindowsTerminal" },
+    @{name = "OpenJS.NodeJS.LTS" },
+    @{name = "Postman.Postman" },
+
+    <# Azure Tooling #>
+    @{name = "Microsoft.AzureCLI" },
+    @{name = "Microsoft.Azure.CosmosEmulator" },
+    @{name = "Microsoft.AzureDataStudio" },
+    @{name = "Microsoft.Azure.StorageEmulator" },
+    @{name = "Microsoft.Azure.StorageExplorer" }
+);
+Foreach ($app in $apps) {
+    $listApp = winget list --exact -q $app.name --accept-source-agreements 
+    if (![String]::Join("", $listApp).Contains($app.name)) {
+        Write-host "Installing:" $app.name
+        if ($app.source -ne $null) {
+            winget install --exact --silent $app.name --source $app.source --accept-package-agreements
+        }
+        else {
+            winget install --exact --silent $app.name --accept-package-agreements
+        }
+    }
+    else {
+        Write-host "Skipping Install of " $app.name
+    }
+}
+
+#Setup WSL
+# wsl --install
